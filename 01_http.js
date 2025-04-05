6456b31065583304d85a68d99fdce17349851934
trisha.olorvida0207 <trisha.olorvida0207@gmail.com>
2025-04-05 05:28:50 +0000

    http

--- 01_http.js
+++ 01_http.js
@@ -1,28 +1,19 @@
 const http = require('https');
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-
-    if (req.url == '/') {
-        const currentDateTime = new Date();
-        const responseObject = {
-            date: currentDateTime.toISOString()
-        };
-        res.writeHead(200);
-        res.end(JSON.stringify(responseObject));
-    }
-
-    else if (req.url == '/hello') {
-        res.writeHead(200);
-        res.end('Hello, World!');
-    }
-
-    else {
-        res.writeHead(404);
-        res.end(JSON.stringify({error: 'Not Found'}));
-    }
+  if (req.url === "/") {
+    const date = new Date();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ date: date.toISOString() }));
+  } else if (req.url === "/hello") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end('Hello, World!');
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Route not found");
+  }
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
-  });
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000...");
+});
