bb8a804d8b92445d8db9ec54192744962ec4c5da
trisha.olorvida0207 <trisha.olorvida0207@gmail.com>
2025-04-05 04:32:42 +0000

    http

---
+++ 01_http.js
@@ -0,0 +1,29 @@
+const http = require('https');
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    if (req.url == '/') {
+        const currentDateTime = new Date();
+        const responseObject = {
+            date: currentDateTime.toISOString(),
+            message: 'Current date and time'
+        };
+        res.writeHead(200);
+        res.end(JSON.stringify(responseObject));
+    }
+
+    else if (req.url == '/hello') {
+        res.writeHead(200);
+        res.end('Hello, World!');
+    }
+
+    else {
+        res.writeHead(404);
+        res.end(JSON.stringify({error: 'Not Found'}));
+    }
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Server running at http://localhost:3000/');
+  });
---
+++ 02_fs.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+// Read a file asynchronously
+fs.readFile('example.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+  } else {
+    console.log('File content:', data);
+  }
+});
